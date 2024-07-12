0b341248e07cd8761fef347644ed0dc85e17f3cc
berithx <seere7777@gmail.com>
2024-07-12 16:38:00 +0900

    [#4] feat: Token Reissue, Logout
    - Token reIssue API 구현
    - Logout API 구현
    - Logout은 프론트에서 AT 삭제, 백에서 RT 삭제

--- src/main/java/com/sparta/tentrillion/security/filter/AuthenticationFilter.java
+++ src/main/java/com/sparta/tentrillion/security/filter/AuthenticationFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -17,6 +19,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class AuthenticationFilter extends OncePerRequestFilter {
 
@@ -34,7 +37,7 @@ public class AuthenticationFilter extends OncePerRequestFilter {
         String accessToken = jwtService.substringToken(accessTokenValue);
 
         if (!jwtService.isTokenValidate(accessToken)) {
-            filterChain.doFilter(request, response);
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return;
         }
 
