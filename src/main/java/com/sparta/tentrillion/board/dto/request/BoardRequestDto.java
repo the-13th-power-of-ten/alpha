f8f7aedae6dbf59a728d913468cb0feaffe716ad
jkhan94 <hjkwork@naver.com>
2024-07-12 12:00:53 +0900

    [#5] feat : 보드 관리 기능 추가
    - 보드 생성
    - 보드 수정
    - 보드 삭제
    - 보드 초대
    - 보드 목록 조회 (초대받은 보드만 조회)
    - 보드에 초대받은 사람만 보드 관리 기능 사용가능 (조회 포함)
    - USER 권한의 사용자는 조회만 가능
    - MANAGER 권한의 사용자는 모든 기능 사용 가능

--- src/main/java/com/sparta/tentrillion/board/dto/request/BoardRequestDto.java
+++ src/main/java/com/sparta/tentrillion/board/dto/request/BoardRequestDto.java
@@ -1,4 +1,18 @@
 package com.sparta.tentrillion.board.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardRequestDto {
+
+    @NotNull(message = "보드 제목을 입력해주세요.")
+    private String title;
+
+    @NotNull(message = "보드 설명을 입력해주세요.")
+    private String description;
 }
---
+++ src/main/java/com/sparta/tentrillion/board/repository/InvitedBoardRepository.java
@@ -0,0 +1,15 @@
+package com.sparta.tentrillion.board.repository;
+
+import com.sparta.tentrillion.board.entity.Board;
+import com.sparta.tentrillion.board.entity.InvitedBoard;
+import com.sparta.tentrillion.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvitedBoardRepository extends JpaRepository<InvitedBoard, Long> {
+
+    InvitedBoard findByBoardAndSenderAndReceiver(Board board, User sender, User receiver);
+
+    InvitedBoard findByBoardAndReceiver(Board board, User sender);
+}
