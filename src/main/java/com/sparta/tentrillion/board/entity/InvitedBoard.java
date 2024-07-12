f87b643c553ea2f15b568174a20c8aa971623b11
jkhan94 <hjkwork@naver.com>
2024-07-12 10:17:03 +0900

    [#5] refactor: InvitedBoards -> InvitedBoard로 이름변경

--- src/main/java/com/sparta/tentrillion/board/entity/InvitedBoard.java
+++ src/main/java/com/sparta/tentrillion/board/entity/InvitedBoard.java
@@ -1,4 +1,4 @@
-package com.sparta.tentrillion.board;
+package com.sparta.tentrillion.board.entity;
 
 import com.sparta.tentrillion.global.TimeStamp;
 import com.sparta.tentrillion.user.User;
@@ -12,7 +12,7 @@ import lombok.*;
         uniqueConstraints = {@UniqueConstraint(name = "UniqueBoardAndUsers", columnNames = {"board_id", "sender_id", "receiver_id"})})
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class InvitedBoards extends TimeStamp {
+public class InvitedBoard extends TimeStamp {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +29,10 @@ public class InvitedBoards extends TimeStamp {
     @ManyToOne
     @JoinColumn(name = "board_id", nullable = false)
     private Board board;
+
+    public InvitedBoard(User sender, User receiver, Board board) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.board = board;
+    }
 }
